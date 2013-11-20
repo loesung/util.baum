da8b3b94d6632a6d47157078370a0a593f46ef45
NeoAtlantis <rijndael@yeah.net>
2013-11-20 16:31:41 +0800

    fix

--- net/packet.js
+++ net/packet.js
@@ -5,7 +5,7 @@ function packet(baum, protocol, request, response){
     this.response = response;
 
     this.url = baum.nodejs.url.parse(this.request.url);
-    this.url.protocol = protocol;
+    this.url.protocol = protocol + ':';
 
     this.write = function(p1, p2){
         self.response.write(p1, p2);
