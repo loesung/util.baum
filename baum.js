3f1844fab5fd76ee90ecccfeb2b511911ebb54ba
NeoAtlantis <rijndael@yeah.net>
2013-11-26 23:29:41 +0800

    add process

--- baum.js
+++ baum.js
@@ -1,4 +1,4 @@
-$ = (function(){
+$ = new function(){
     var self = this;
 
     var desired = {
@@ -8,10 +8,11 @@ $ = (function(){
         'net': './net/__init__.js',
         'config': './config/__init__.js',
         'global': './global/__init__.js',
+        'process': './process/__init__.js',
     };
 
     for(var name in desired)
         this[name] = require(desired[name])(self);
 
     return this;
-})();
+};
