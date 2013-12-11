0b16175e6e9e5364ede640bfdcc10a4056cf61aa
NeoAtlantis <rijndael@yeah.net>
2013-12-12 01:05:57 +0800

    add child_process inclusion

--- nodejs/__init__.js
+++ nodejs/__init__.js
@@ -14,6 +14,7 @@ module.exports = function(baum){
             'buffer',
             'events',
             'querystring',
+            'child_process',
 
             'async',
             'buffalo',
